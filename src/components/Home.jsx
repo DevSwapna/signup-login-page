4157cd9343d0edc6b899d4cbcb5b0dba762bad6e
SwapnaSahoo <sahooswapnaswarup@gmail.com>
2025-02-09 17:35:25 +0530

    feat: Add navbar and buttons for login/signup functionality
    Added a navbar with buttons in the home area to enable user actions like login and signup.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,23 @@
 import "./Home.css";
+import HomeNavbar from "./HomeNavbar";
 
 function Home() {
   return (
     <>
+      <HomeNavbar />
       <main>
         <div className="container">
           <h1 className="heading">GET STARTED AWAY</h1>
           <h3 className="home-text2">Learn With Us</h3>
           <h4 className="home-text3">By SwapnaSahoo</h4>
+          <div className="lgoin-signup">
+            <a href="#" className="login">
+              LOGIN
+            </a>
+            <a href="#" className="signup">
+              SIGNUP
+            </a>
+          </div>
         </div>
       </main>
     </>
--- src/components/HomeNavbar.jsx
+++ src/components/HomeNavbar.jsx
@@ -1,5 +1,23 @@
 import "./HomeNavbar.css";
 
-function HomeNavbar() {}
+function HomeNavbar() {
+  return (
+    <>
+      <header className="header">
+        <nav className="navbar">
+          <img src="src/assets/logo.png" alt="logo" className="navbar-logo" width={50}/>
+          <span className="swapna">Swapna</span>
+          <span className="sahoo">Sahoo</span>
+          <div className="links">
+          <a href="#" className="home">Home</a>
+          <a href="#" className="github">GitHub</a>
+          <a href="#" className="projects">Projects</a>
+          <a href="#" className="signup">SIGNUP</a>
+          </div>
+        </nav>
+      </header>
+    </>
+  );
+}
 
 export default HomeNavbar;
